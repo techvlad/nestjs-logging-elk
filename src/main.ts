4391ec9663425e9d26cc82b03924c70c49ec3991
Vlad Krokhin <3v1lh3x@gmail.com>
2020-02-07 14:03:12 +0200

    add error loggin

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,36 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 
+const signalsNames: NodeJS.Signals[] = ['SIGTERM', 'SIGINT', 'SIGHUP'];
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     logger: false,
   });
-  app.useLogger(app.get(Logger));
+
+  const logger = app.get(Logger);
+
+  signalsNames.forEach(signalName =>
+    process.on(signalName, signal => {
+      logger.log(`Retrieved signal: ${signal}, application terminated`);
+      process.exit(0);
+    }),
+  );
+
+  process.on('uncaughtException', (error: Error) => {
+    logger.error({ err: error });
+    process.exit(1);
+  });
+
+  process.on('unhandledRejection', (reason, promise) => {
+    logger.error(`Unhandled Promise Rejection, reason: ${reason}`);
+    promise.catch((err: Error) => {
+      logger.error({ err });
+      process.exit(1);
+    });
+  });
+
+  app.useLogger(logger);
   app.disable('x-powered-by');
   await app.listen(3000);
 }
