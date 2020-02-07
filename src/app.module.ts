3d74d239996a086413fb6fe7ca7280b38256dcc0
Vlad Krokhin <3v1lh3x@gmail.com>
2020-02-07 14:23:40 +0200

    replace unixtime stamps to iso time string

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { LoggerModule } from 'nestjs-pino';
+import { stdTimeFunctions } from 'pino';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -7,6 +8,7 @@ import { AppService } from './app.service';
   imports: [
     LoggerModule.forRoot({
       pinoHttp: {
+        timestamp: stdTimeFunctions.isoTime,
         level: process.env.NODE_ENV !== 'production' ? 'trace' : 'info',
         prettyPrint: process.env.NODE_ENV !== 'production',
         useLevelLabels: true,
